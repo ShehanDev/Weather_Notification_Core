9dac748238f25258926eaba62a133bc28b1b9dd3
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2024-01-12 17:40:09 +0530

    style : add email  styles

--- services/mailService.js
+++ services/mailService.js
@@ -17,7 +17,25 @@ export const sendWeatherReport = async (email, city, fetchWeatherData) => {
       from: process.env.USER_EMAIL,
       to: email,
       subject: "Hourly Weather Report",
-      text: `Temperature: ${weatherData.temperature}°C, Description: ${weatherData.description}`,
+      // text: `Temperature: ${weatherData.temperature}°C, Description: ${weatherData.description}`,
+      html: `
+      <html>
+        <body>
+          <div style="font-family: Arial, sans-serif; padding: 20px;">
+            <h2>Hourly Weather Report</h2>
+            <p>Dear User,</p>
+            <p>Here is the latest weather report for location : ${city}:</p>
+            <ul>
+              <li><strong>Temperature:</strong> ${weatherData.temperature}°C</li>
+              <li><strong>Description:</strong> ${weatherData.description}</li>
+            </ul>
+            <p>Stay informed and have a great day!</p>
+            <p>Best regards,</p>
+            <p>Your Weather Notification Service</p>
+          </div>
+        </body>
+      </html>
+    `,
     };
 
     await transporter.sendMail(mailOptions);
