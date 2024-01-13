7deb9d3d0468c78534c744d55a9b9f42ac87ec6d
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2024-01-13 19:00:59 +0530

    refactor : index and route files

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.js";
 dotenv.config();
-
 const app = express();
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json());
 app.use(cors());
 
 const CONNECTION_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 5000;
 
-//db connection
+//database connection
 mongoose
   .connect(CONNECTION_URL)
   .then(() => console.log("connected to Database!"))
@@ -23,11 +20,13 @@ mongoose
     console.log(err);
   });
 
-app.use("/users", userRoutes);
-
+//routes
+//base  route
 app.get("/", (req, res) => {
   res.send("Home api");
 });
+//user route
+app.use("/users", userRoutes);
 
 //start server
 app.listen(PORT, () => console.log(`server up and Running at ${PORT} `));
--- routes/user.js
+++ routes/user.js
@@ -7,19 +7,23 @@ import {
   getWeatherByDate,
   activeNotification,
 } from "../controllers/userController.js";
-
 import { login } from "../services/auth.js";
 import { verifyToken } from "../services/auth.js";
-
 //initializing routes
 const router = express.Router();
+
 //user login route
 router.post("/login", login);
 //get all user details with weather
 router.get("/all", getUsers);
+//get user details by id
 router.get("getUser/:id", getUserById);
+//create users
 router.post("/register", createUsers);
-router.put("/:id", verifyToken, updateCity);
+// Update user's city by ID
+router.put("/update/:id", verifyToken, updateCity);
+// Get user's weather data for a specific date
 router.get("/:id/weather/:date", verifyToken, getWeatherByDate);
-router.post("/:id/activate-notification", verifyToken, activeNotification);
+// Activate email notifications for users and send weather updates every 3 hours
+router.post("/email/activate/:id", verifyToken, activeNotification);
 export default router;
